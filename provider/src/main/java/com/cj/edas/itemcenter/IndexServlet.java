f5a070b7d1153a30320404964d77356f132d66ab
CaoZejun <jun5352@126.com>
2017-09-30 09:34:50 +0800

    provider implements api
    The server implements the interface and publishes the service

---
+++ provider/src/main/java/com/cj/edas/itemcenter/IndexServlet.java
@@ -0,0 +1,29 @@
+package com.cj.edas.itemcenter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = resp.getWriter();
+		pw.write("ItemCenter - OK");
+		return;
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+
+}
